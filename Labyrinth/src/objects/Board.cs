ae327debccb25e58af366069428c60693a3b3091
MatthewDietrich <mbd321123@gmail.com>
2018-05-23 14:20:29 -0500

    Add BufferGenerator class to clean up Board class

--- Labyrinth/src/objects/Board.cs
+++ Labyrinth/src/objects/Board.cs
@@ -47,54 +47,12 @@ namespace Labyrinth
 
             // This is all tentative
             // Vertex list
-            ColoredVertex[] vertices =
-            {
-                // front
-                new ColoredVertex(new Vector3(leftBound, bottomBound, frontDepth), Color.BlanchedAlmond),
-                new ColoredVertex(new Vector3(rightBound, bottomBound, frontDepth), Color.BlanchedAlmond),
-                new ColoredVertex(new Vector3(rightBound, topBound, frontDepth), Color.BlanchedAlmond),
-                new ColoredVertex(new Vector3(leftBound, topBound, frontDepth), Color.BlanchedAlmond),
-
-                // back
-                new ColoredVertex(new Vector3(leftBound, bottomBound, backDepth), Color.Brown),
-                new ColoredVertex(new Vector3(leftBound, topBound, backDepth), Color.Brown),
-                new ColoredVertex(new Vector3(rightBound, topBound, backDepth), Color.Brown),
-                new ColoredVertex(new Vector3(rightBound, bottomBound, backDepth), Color.Brown),
-
-                // top
-                new ColoredVertex(new Vector3(leftBound, topBound, backDepth), Color.Blue),
-                new ColoredVertex(new Vector3(leftBound, topBound, frontDepth), Color.Blue),
-                new ColoredVertex(new Vector3(rightBound, topBound, frontDepth), Color.Blue),
-                new ColoredVertex(new Vector3(rightBound, topBound, backDepth), Color.Blue),
-
-                // bottom
-                new ColoredVertex(new Vector3(leftBound, bottomBound, backDepth), Color.MediumOrchid),
-                new ColoredVertex(new Vector3(rightBound, bottomBound, backDepth), Color.MediumOrchid),
-                new ColoredVertex(new Vector3(rightBound, bottomBound, frontDepth), Color.MediumOrchid),
-                new ColoredVertex(new Vector3(topBound, bottomBound, frontDepth), Color.MediumOrchid),
-
-                // right
-                new ColoredVertex(new Vector3(rightBound, topBound, backDepth), Color.Gray),
-                new ColoredVertex(new Vector3(rightBound, topBound, frontDepth), Color.Gray),
-                new ColoredVertex(new Vector3(rightBound, bottomBound, frontDepth), Color.Gray),
-                new ColoredVertex(new Vector3(rightBound, bottomBound, backDepth), Color.Gray),
-
-                // left
-                new ColoredVertex(new Vector3(leftBound, bottomBound, backDepth), Color.Magenta),
-                new ColoredVertex(new Vector3(leftBound, bottomBound, frontDepth), Color.Magenta),
-                new ColoredVertex(new Vector3(leftBound, topBound, frontDepth), Color.Magenta),
-                new ColoredVertex(new Vector3(leftBound, topBound, backDepth), Color.Magenta)
-            };
+            ColoredVertex[] vertices = BufferGenerator.CubeVertices(
+                frontDepth, backDepth, topBound, bottomBound, rightBound, leftBound,
+                Color.AliceBlue, Color.AntiqueWhite, Color.Aqua, Color.Bisque, Color.Thistle, Color.SpringGreen);
 
             // Index list
-            uint[] indices = {
-                0, 1, 2, 0, 2, 3,    // front
-                4, 5, 6, 4, 6, 7,    // back
-                8, 9, 10, 8, 10, 11,   // top
-                12, 13, 14, 12, 14, 15,   // bottom
-                16, 17, 18, 16, 18, 19,   // right
-                20, 21, 22, 20, 22, 23   // left
-            };
+            uint[] indices = BufferGenerator.CubeIndices();
 
             // Add lists to buffers
             VBuffer.AddVertices(vertices);
